e01d03e0b9b939970c65ef94c3edba0899fe929f
Sabarish-2 <sabarishh03@gmail.com>
2024-01-17 11:03:09 +0530

    Refactor code to use request.args.get() instead of request.args.get. Add placeholder text for name input field.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # name = "World!"
-    if name in request.args.get:
+    if name in request.args:
         name = request.args.get('name')
         return render_template('greet.html', name = name)
     return render_template('index.html')
