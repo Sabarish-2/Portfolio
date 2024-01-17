3e1c9294fd56851d33dddb99851db5db275b604a
Sabarish-2 <118958969+Sabarish-2@users.noreply.github.com>
2024-01-17 20:27:58 +0530

    90s

--- static/animate.js
+++ static/animate.js
@@ -16,8 +16,8 @@ document.addEventListener("DOMContentLoaded", function() {
     } catch (error) { }
 });
 
-// setTimeout(function() {
-//     document.querySelectorAll('.delayed-img').forEach(img => {
-//         img.src = img.dataset.src;
-//     });
-// }, 120);
+setTimeout(function() {
+    document.querySelectorAll('.delayed-img').forEach(img => {
+        img.src = img.dataset.src;
+    });
+}, 90);
