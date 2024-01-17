585cc99e379449ae225d4e77fad087a11197f26e
Sabarish-2 <sabarishh03@gmail.com>
2024-01-17 10:52:14 +0530

    Add route for greeting users

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, user!'
+def index():
+    # name = "World!"
+    if name in request.args.get:
+        name = request.args.get('name')
+        return render_template('greet.html', name = name)
+    return render_template('index.html')
 
 # if __name__ == '__main__':
 #     app.run()
